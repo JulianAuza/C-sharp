a9788b8eb45c1e6af28ba9c77807fba37905939f
JulianAuza <julianauza@gmail.com>
2017-09-05 13:40:41 -0500

    first commit

---
+++ fundamentalsI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace fundamentalsI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for( int i=1; i<21;i++){
+                if(i%3==0&&i%5==0){
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i%3==0){
+                    Console.WriteLine("Fizz");
+                }
+                else if(i%5==0){
+                    Console.WriteLine("Buzz");
+                }
+                else{
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+    }
+}
